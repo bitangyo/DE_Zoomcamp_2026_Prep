1f1eeebfe30eea48d157970dcab656a04d5e1e35
bitangyo <polgarlukacs@gmail.com>
2025-12-09 22:08:45 +0100

    I don't know what a commit message should contain, will look into the freecodecamp git tutorial

---
+++ week1/hello.py
@@ -0,0 +1,8 @@
+# My first Python practice for DE Zoomcamp prep
+print("Hello, Data Engineering!")
+
+# Basic variables
+name = "bitangyo"
+weeks_until_zoomcamp = 4
+
+print(f"My name is {name} and I have {weeks_until_zoomcamp} weeks to prepare!")
